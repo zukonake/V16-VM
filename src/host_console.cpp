610218c726598711b1e13f5d8ec8cac6ffe69298
zukonake <zukonake@protonmail.com>
2017-11-26 00:34:50 +0100

    Host console now works on both SDP and ADP

--- src/host_console.cpp
+++ src/host_console.cpp
@@ -2,14 +2,29 @@
 //
 #include "host_console.hpp"
 
+HostConsole::HostConsole()
+{
+}
+
+Word &HostConsole::handleSdp(Word value)
+{
+	if(value >= BUFFER_SIZE)
+	{
+		throw std::out_of_range("buffer address: " + std::to_string(value) + " is out of range");
+	}
+	return reinterpret_cast<Word &>(buffer[value]);
+}
+
 void HostConsole::handleAdpI(Word value)
 {
-	Command command = static_cast<Command>((value & 0xFF00) >> 8);
+	Command command = static_cast<Command>(value);
 	switch(command)
 	{
 		case NOP: break;
-		case APD: buffer += static_cast<char>(value & 0x00FF); break;
-		case WRT: std::cout << buffer; break;
-		case CLR: buffer = ""; break;
+		case WRT: std::cout << reinterpret_cast<char const *>(buffer); break;
+		case CLR:
+			for(unsigned i = 0; i < BUFFER_SIZE; i++) buffer[i] = 0x00;
+			bufferIterator = 0;
+			break;
 	}
 }
--- src/host_console.hpp
+++ src/host_console.hpp
@@ -1,24 +1,29 @@
 #pragma once
 
+#include <cstddef>
 #include <string>
-#include <v16/architecture.hpp>
 //
+#include <v16/architecture.hpp>
 #include <hardware.hpp>
 
 class HostConsole : public Hardware
 {
 	public:
+	static std::size_t const BUFFER_SIZE = 0x80;
 	enum Command
 	{
 		NOP = 0x00,
-		APD = 0x01,
-		WRT = 0x02,
-		CLR = 0x03,
+		WRT = 0x01,
+		CLR = 0x02,
 	};
 
+	HostConsole();
+
 	protected:
+	virtual Word &handleSdp(Word value) override;
 	virtual void handleAdpI(Word value) override;
 
-	std::string buffer;
+	Word buffer[BUFFER_SIZE];
+	std::size_t bufferIterator;
 };
 
