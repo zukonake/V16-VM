32202f66f88e39046b7da0b59948424fd62be8f5
zukonake <zukonake@protonmail.com>
2017-11-25 21:01:31 +0100

    ROM path is now loaded from args

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
+#include <iostream>
+//
 #include <rom.hpp>
 #include <memory.hpp>
 #include <host_console.hpp>
 #include <cpu.hpp>
 
-int main()
+int main(int argc, char *argv[])
 {
+	if(argc < 2)
+	{
+		std::cout << "usage: " << argv[0] << " ROM_PATH\n";
+		return 1;
+	}
+	std::string romPath = argv[1];
+
 	Memory<0x10000> memory;
-	ROM<0x100> rom("test.bin");
+	ROM<0x100> rom(romPath);
 	HostConsole console;
 
 	CPU cpu;
